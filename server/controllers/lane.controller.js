e62c5bbb4dfd08f6dfa701edc98505b393e8b1fe
Artur <akaluza26@gmail.com>
2018-03-10 11:27:05 +0100

    Delete lane

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -30,3 +30,16 @@ export function getLanes(req, res) {
     res.json({ lanes });
   });
 }
+
+// delete lane
+export function deleteLane(req, res) {
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
