9734d5190526752679e35a3c07bed2a57b44abd1
Artur <akaluza26@gmail.com>
2018-03-10 12:30:06 +0100

    Fix add note

--- server/models/lane.js
+++ server/models/lane.js
@@ -5,6 +5,9 @@ const laneSchema = new Schema({
   name: { type: 'String', required: true },
   notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
   id: { type: 'String', required: true, unique: true },
-});
+}, 
+  {
+    usePushEach: true, 
+  });
 
 export default mongoose.model('Lane', laneSchema);
